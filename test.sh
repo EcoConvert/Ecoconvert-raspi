d768db2375829ce4f598a89803b4131c5d3a7ac2
dahy14 <naypes.15@gmail.com>
2024-10-04 16:01:06 +0800

    Test.sh ok

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 # Define the repository URL and the branch you want to fetch
 REPO_URL="https://github.com/EcoConvert/Ecoconvert-raspi.git"
 BRANCH_NAME="deployment"
-DIR_NAME="./Ecoconvert-raspi"
+DIR_NAME="./"
 
 # Check if the directory already exists
 if [ -d "$DIR_NAME" ]; then
   echo "Directory $DIR_NAME already exists. Fetching updates from branch '$BRANCH_NAME'..."
   cd "$DIR_NAME" || exit
-  git fetch origin "$BRANCH_NAME:$BRANCH_NAME"  # Fetch the specific branch
+  git pull origin "$BRANCH_NAME:$BRANCH_NAME"  # Fetch the specific branch
 else
   echo "Cloning the repository..."
   git clone -b "$BRANCH_NAME" "$REPO_URL" "$DIR_NAME"
