d4c9f41f43bab7ee041136631761653432a3ff2f
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2024-10-14 15:08:44 +0800

    try

--- process/inserts.py
+++ process/inserts.py
@@ -1,103 +1,63 @@
 import RPi.GPIO as GPIO
-#from hx711 import HX711
 from modules.Camera import Camera
 from gpiozero import Servo
 from time import sleep
 
-
-#Instances
+# Instances
 leftServo = Servo(18)
 rightServo = Servo(26)
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN)  # Set GPIO 5 as input to detect the parameter
 
 hx = HX711(dout_pin=6, pd_sck_pin=5)
 
-
-def set_servo_angle(angle):
-	servo_value = (angle - 90) / 90
-	servo.value = servo_value
-	print (f"Servo moved to {angle} degrees")
-	sleep(1)                                                                          
-
+def set_servo_angle(servo, angle):
+    servo_value = (angle - 90) / 90
+    servo.value = servo_value
+    print(f"Servo moved to {angle} degrees")
+    sleep(1)
 
 def test_capture():
     cam = Camera(0)
     cam.init_camera()
     
     labels = cam.load_labels()
-    print(labels)   
+    print(labels)
 
     inference = cam.capture_and_infer()
     print(inference)
-	
+
     if inference == "Object: plastic":
-        set_servo_angle(0)
+        set_servo_angle(leftServo, 0)
         sleep(2)
     elif inference == "Object: glass":
-        set_servo_angle(0)
+        set_servo_angle(leftServo, 90)
         sleep(2)
     elif inference == "Object: metal":
-        set_servo_angle(0)
+        set_servo_angle(leftServo, 180)
         sleep(2)
-    elif inference == "Object: cardboard_paper":
-        set_servo_angle(0)
-        sleep(2)
-    elif inference == "Object: rock":
-        set_servo_angle(0)
-        sleep(2)
-    elif inference == "Object: trash":
-        set_servo_angle(0)
-        sleep(2)
-    elif inference == "Object: PET bottle 1.5L":
-        set_servo_angle(leftServo, 90)
-        sleep(2)
-    elif inference == "Object: Not 1.5L":
-        set_servo_angle(leftServo, 90)
-        sleep(2)
-    elif inference == "Object: Crumpled":
-        set_servo_angle(0)
-        sleep(2)
-    elif inference == "Object: Capped":
-        set_servo_angle(0)
-        sleep(2)
-    elif inference == "Object: Unclean":
-        set_servo_angle(0)
-        sleep(2)
+    # Add other inferences as needed
     elif inference == "Object: leaf":
-        set_servo_angle(0)
+        set_servo_angle(rightServo, 90)
         sleep(2)
-        
-		
-    
+
 #-------------------------RUN
 try:
-	while True:
-		reading = hx.get_raw_data_mean()
-		print(reading)
-		
-		
-		set_servo_angle(leftServo, 0)
-		sleep(2)
-		
-		set_servo_angle(leftServo, 90)
-		sleep(2)
-		
-		set_servo_angle(leftServo, 180)
-		sleep(2)
-		
-		# servo2
-		
-		set_servo_angle(rightServo, 0)
-		sleep(2)
-		
-		set_servo_angle(rightServo, 90)
-		sleep(2)
-		
-		set_servo_angle(rightServo, 180)
-		sleep(2)
-		
-except KeyboardInterrupt:
-	print("Program stopped")
+    while True:
+        reading = hx.get_raw_data_mean()
+        print(reading)
 
-if __name__ == "__main__":
-    test_capture()
+        # Check if the specific parameter is detected on GPIO 5
+        if GPIO.input(5) == GPIO.HIGH:  # Parameter detected
+            print("Parameter detected on GPIO 5")
+            
+            # Run the test_capture function to infer object and move the servo
+            test_capture()
+        else:
+            print("No parameter detected on GPIO 5")
+            sleep(1)
+
+except KeyboardInterrupt:
+    print("Program stopped")
+finally:
+    GPIO.cleanup()  # Clean up the GPIO pins when exiting
