8afec764ca1b08a01a1eeb98cce7bb4aa36738cc
Ronald Javalde <rjavalde9@gmail.com>
2024-11-22 12:44:51 +0800

    refactor(encode and decode): user input points for encode and remove redis in decode

--- draft_jwt.py
+++ draft_jwt.py
@@ -1,11 +1,8 @@
 import datetime
 import os
-import time
-import uuid
 
 import jwt
 import qrcode
-import redis
 from dotenv import load_dotenv
 from PIL import Image
 from pyzbar.pyzbar import decode
@@ -16,18 +13,18 @@ load_dotenv()
 # Load secret key
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# Setup Redis
-redis_client = redis.StrictRedis(host="localhost", port=6379, decode_responses=True)
-
-
 # Revert time if need
 # time_convert = datetime.datetime.fromtimestamp(payload["iat"]).strftime("%Y-%m-%d %H:%M:%S")
 
 
 # Empty parameters for now, in the future, it will need to get the points
-def encode_jwt():
-    payload = {"points": 50, "iat": int(datetime.datetime.now().timestamp()), "jti": str(uuid.uuid4())}
+def encode_jwt(point: int):
+    payload = {
+        "points": point,
+        "iat": int(datetime.datetime.now().timestamp()),
+    }
     valid_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    print(payload)
 
     # Generate QR Code
     qr = qrcode.make(valid_token)
@@ -42,7 +39,6 @@ def encode_jwt():
 def decode_and_validate(qr_image_path: str):
     # Decode the image
     qr_image = Image.open(qr_image_path)
-
     # Get the data and convert to string
     decoded_data = decode(qr_image)[0].data.decode("utf-8")
 
@@ -50,28 +46,21 @@ def decode_and_validate(qr_image_path: str):
     try:
         # Decode the jwt
         decoded = jwt.decode(decoded_data, SECRET_KEY, algorithms=["HS256"])
-
-        # Get the 'jti' then check if the points is already redeemed
-        jti = decoded["jti"]
-        print(str(jti))
-
-        # Check if the token is already redeemed
-        if redis_client.exists(jti):
-            return "Token already redeemed"
-        else:
-            # Mark the token as redeem, changing the JTI 7 days try
-            # Subject to change, can be a month
-            redis_client.setex(jti, 7 * 24 * 60 * 60, "redeemed")
-            return "Points redeemed successfully"
-
+        return {"status": "Success", "decoded_token": decoded}
     except jwt.InvalidTokenError:
-        return "Invalid Token"
+        return {"status": "Error", "message": "Token has expired"}
     except jwt.ExpiredSignatureError:
-        return "Token has expired"
+        return {"status": "Error", "message": "Invalid Token"}
 
 
 # Generate JWT and QR
 # a = encode_jwt()
 # print(a)
 
-print(decode_and_validate("token.png"))
+
+if __name__ == "__main__":
+    points_input = int(input("How many points: "))
+    a = encode_jwt(points_input)
+
+    b = decode_and_validate(a)
+    print(b)
