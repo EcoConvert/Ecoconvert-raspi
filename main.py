10b7c5c4213ba41d746d94e4de507f956a5bd4aa
javaldrnld <rjavalde9@gmail.com>
2024-11-30 13:50:30 +0800

    feat: initial commit

--- main.py
+++
@@ -1,9 +0,0 @@
-import logging
-from logging_config import setup_logging 
-from process.inserts import test_load_labels
-# from process.blink import blink
-
-setup_logging()
-
-# blink()
-
--- upd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Updating requirements.txt"
-source env/bin/activate
-python -m pip freeze >requirements.txt
